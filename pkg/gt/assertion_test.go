dd879ca8d12a88209edc733efcad176c6e35d595
yrichika <68413973+yrichika@users.noreply.github.com>
2024-02-21 20:42:48 +0900

    Refactor assertion tests and add time and person comparisons

--- pkg/gt/assertion_test.go
+++ pkg/gt/assertion_test.go
@@ -2,7 +2,6 @@ package gt
 
 import (
 	"testing"
-	"time"
 )
 
 type Person struct {
@@ -22,47 +21,6 @@ func TestExpectAssertions(testingT *testing.T) {
 		t.It("should pass when value is NOT nil", func() {
 			notNilValue := 1
 			Expect(t, &notNilValue).Not().ToBeNil()
-		})
-	})
-
-	t2 := CreateTest(testingT)
-	t2.Describe("ToBe", func() {
-		t2.It("should pass when two values are equal", func() {
-			// not all primitive types are tested,
-			// but it's enough to test the functionality
-			var intVal1 int = 1
-			var intVal2 int = 1
-			Expect(t2, &intVal1).ToBe(intVal2)
-
-			var strVal1 string = "hoge"
-			var strVal2 string = "hoge"
-			Expect(t2, &strVal1).ToBe(strVal2)
-
-			var boolVal1 bool = true
-			var boolVal2 bool = true
-			Expect(t2, &boolVal1).ToBe(boolVal2)
-
-			var floatVal1 float64 = 1.1
-			var floatVal2 float64 = 1.1
-			Expect(t2, &floatVal1).ToBe(floatVal2)
-
-			var complexVal1 complex128 = 1 + 1i
-			var complexVal2 complex128 = 1 + 1i
-			Expect(t2, &complexVal1).ToBe(complexVal2)
-
-			time1 := time.Now()
-			time2 := time1
-			Expect(t2, &time1).ToBe(time2)
-
-			person1 := Person{Name: "hoge", Age: 1}
-			person2 := Person{Name: "hoge", Age: 1}
-			Expect(t2, &person1).ToBe(person2)
-		})
-
-		t2.It("should pass when two values are NOT equal", func() {
-			var intVal1 int = 1
-			var intVal2 int = 2
-			Expect(t2, &intVal1).Not().ToBe(intVal2)
 		})
 	})
 
